87a008df3d0b2327243d4725766b449202eb62fd
prabhsembhi <prabhpreetsingh013@gmail.com>
2020-05-20 01:04:17 -0700

    Updates

---
+++ precache-manifest.7237d7232c6f014e839b8c193ebea574.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c920212f980f010c7ca97d75375a181c",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "c5d486a76f2e5d2f19d3",
+    "url": "/react-practice/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd6481eb5194d0fe544a",
+    "url": "/react-practice/static/js/2.a95dc5dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-practice/static/js/2.a95dc5dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5d486a76f2e5d2f19d3",
+    "url": "/react-practice/static/js/main.ff97c85f.chunk.js"
+  },
+  {
+    "revision": "00c2fbd79e873ff2e6bd",
+    "url": "/react-practice/static/js/runtime-main.f1a04a25.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-practice/static/media/logo.5d5d9eef.svg"
+  }
+]);
